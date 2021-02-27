06d33cfd1f265925250a38c40fbbf2fe79843d6a
hoangnb <hoangnb@gearinc.com>
2021-02-27 18:01:07 +0700

    fix

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.post("/get-gacha-logs", async (req, res) => {
   res.send(result);
 });
 
-app.post("/load-data", async (req, res)) => {
-  const {userKey} = req.body;
+app.post("/load-data", async (req, res) => {
+  const { userKey } = req.body;
   console.log(userKey);
   let result = await GenshinGachaServices.loadUserData(userKey);
-  res.send(result); 
-}
+  res.send(result);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
